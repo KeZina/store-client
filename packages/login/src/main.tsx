9e66040766028f4930b55485cd5f5aef987fd460
KeZina <kezina2036@gmail.com>
2023-04-24 12:40:16 +0600

    initial

---
+++ packages/login/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { App } from './components/app';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
